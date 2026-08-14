885b80573907f4c0113c488312fa98b04c794b74
designerlyra <228583228+designerlyra@users.noreply.github.com>
2026-08-14 07:27:42 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-# 1. Instala o banco se estiver limpo (ou ignora se ja existir)
-./listmonk --install --idempotent --yes --config ""
 
-# 2. Inicia o servidor web do Listmonk na porta 9000
-./listmonk --config ""
+# Monta o config.toml puxando 100% das Variaveis de Ambiente do Render
+cat <<EOF > config.toml
+[app]
+address = "0.0.0.0:9000"
+
+[db]
+host = "${LISTMONK_db__host}"
+port = ${LISTMONK_db__port}
+user = "${LISTMONK_db__user}"
+password = "${LISTMONK_db__password}"
+database = "${LISTMONK_db__database}"
+ssl_mode = "${LISTMONK_db__ssl_mode}"
+params = "${LISTMONK_db__params}"
+EOF
+
+# Executa a instalacao e inicia o servidor
+./listmonk --install --idempotent --yes --config config.toml
+./listmonk --config config.toml
