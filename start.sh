21fdcd0488e2f62ecd08b65c9a3f8497978d4b97
designerlyra <228583228+designerlyra@users.noreply.github.com>
2026-08-14 06:43:34 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# 1. Instala o banco se estiver limpo (ou ignora se ja existir)
+./listmonk --install --idempotent --yes
+
+# 2. Inicia o servidor web do Listmonk na porta 9000
+./listmonk
