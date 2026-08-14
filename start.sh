52e6390707ecd817aa27eaff02c24616e39b5437
designerlyra <228583228+designerlyra@users.noreply.github.com>
2026-08-14 07:37:01 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,13 @@
 #!/bin/sh
 
-# Imprime no log do Render a confirmacao das variaveis recebidas (sem mostrar a senha)
-echo "=== TESTE DE CREDENCIAIS ==="
+# Remove qualquer arquivo config.toml do disco
+rm -f config.toml
+
+echo "=== INICIANDO LISTMONK PURAMENTE VIA VARIAVEIS DE AMBIENTE ==="
 echo "Host: $LISTMONK_db__host"
-echo "Porta: $LISTMONK_db__port"
 echo "Usuario: $LISTMONK_db__user"
-echo "Banco: $LISTMONK_db__database"
-echo "Params: $LISTMONK_db__params"
-echo "==========================="
+echo "=============================================================="
 
-cat <<EOF > config.toml
-[app]
-address = "0.0.0.0:9000"
-
-[db]
-host = "${LISTMONK_db__host}"
-port = ${LISTMONK_db__port}
-user = "${LISTMONK_db__user}"
-password = "${LISTMONK_db__password}"
-database = "${LISTMONK_db__database}"
-ssl_mode = "${LISTMONK_db__ssl_mode}"
-params = "${LISTMONK_db__params}"
-EOF
-
-./listmonk --install --idempotent --yes --config config.toml
-./listmonk --config config.toml
+# Executa puramente via variaveis de ambiente
+./listmonk --install --idempotent --yes --config ""
+./listmonk --config ""
