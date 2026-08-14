06531d9c94f12aee18acf786d6fd74ed254f6c66
designerlyra <228583228+designerlyra@users.noreply.github.com>
2026-08-14 07:53:08 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-# Remove qualquer arquivo config.toml do disco
-rm -f config.toml
+# Cria o config.toml com sintaxe literal do TOML ('''
+cat <<EOF > config.toml
+[app]
+address = "0.0.0.0:9000"
 
-echo "=== INICIANDO LISTMONK PURAMENTE VIA VARIAVEIS DE AMBIENTE ==="
-echo "Host: $LISTMONK_db__host"
-echo "Usuario: $LISTMONK_db__user"
-echo "=============================================================="
+[db]
+host = "${LISTMONK_db__host}"
+port = ${LISTMONK_db__port}
+user = "${LISTMONK_db__user}"
+password = '''${LISTMONK_db__password}'''
+database = "${LISTMONK_db__database}"
+ssl_mode = "${LISTMONK_db__ssl_mode}"
+params = "${LISTMONK_db__params}"
+EOF
 
-# Executa puramente via variaveis de ambiente
-./listmonk --install --idempotent --yes --config ""
-./listmonk --config ""
+echo "=== ARQUIVO CONFIG.TOML GERADO (SEM MOSTRAR SENHA) ==="
+grep -v "password" config.toml
+echo "======================================================"
+
+./listmonk --install --idempotent --yes --config config.toml
+./listmonk --config config.toml
